6d0268854eb752e8819f413648e9966193ef3e25
chriswebdv <chriswebdv@gmail.com>
2024-05-25 14:56:26 -0600

    Create various types of varables using PHP

---
+++ proj1/index.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Bruce";
+    $food = "pizza";
+    $age = 21;
+    $gpa = 2.5;
+    echo"you are {$age} years old <br>";
+    echo"Hello {$name} <br>";
+    echo"you like {$food} and you gpa is {$gpa}";
+?>
