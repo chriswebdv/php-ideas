1157921b27073b3eac5b3fa4134f9421d7e4846c
chriswebdv <chriswebdv@gmail.com>
2024-05-25 15:25:49 -0600

    Add get method to return users name and password

--- proj2/index.php
+++ proj2/index.php
@@ -1,16 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+        <label>username</label><br>
+        <input type="text" name="username"><br>
+        <label>password</label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Log in">
+    </form>
+</body>
+</html>
+
 <?php
-//Arithmetic operators
+
+echo "{$_GET["username"]} <br>";
+echo "{$_GET["password"]} <br>";
+
 
 $x = 10;
 $y = 3;
 $z = null;
-
 $z = $x + $y;
 $z = $x - $y;
 $z = $x * $y;
 $z = $x / $y;
 $z = $x % $y;
 
-echo $z;
+
 
 ?>
